db6d913e6fc6e5bbd9aec0446b9dc49563491e97
YashUGShejwal <yashugshejwal@gmail.com>
2025-06-15 12:30:21 +0530

    formatting

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -5,7 +5,7 @@ import { ThemeProvider } from '@/contexts/ThemeContext'
 import { AuthProvider } from '@/contexts/AuthContext'
 import { Toaster } from 'sonner'
 
-export function Providers({ children }: { children: React.ReactNode }) {
+export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <SessionProvider>
       <ThemeProvider>
